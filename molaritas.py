834006dffb991170f6af50e2fda17aa6a927aa88
saviladrn <132325574+saviladrn@users.noreply.github.com>
2023-05-05 16:08:16 +0700

    Update molaritas.py

--- molaritas.py
+++ molaritas.py
@@ -5,14 +5,15 @@ st.title("Aplikasi Menghitung Molaritas")
 sampel = st.text_input("Masukkan nama bahan baku sekunder")
 penitar = st.text_input("Masukkan nama bahan baku primer")
 
-bobot = st.number_input("Masukkan nilai bobot zat bahan baku primer (gram)")
+bobot = st.number_input("Masukkan nilai bobot zat bahan baku primer (mg)")
 volume = st.number_input("Masukkan volume larutan bahan baku sekunder (mL)")
 mr = st.number_input("Masukkan nilai Mr bahan baku primer (g/mol)")
+fp = st.number_input("Masukkan nilai faktor pengenceran")
 
 tombol = st.button("Hitung nilai Molaritas")
 
 if tombol:
-    molaritas=bobot/(mr*volume)
+    molaritas=bobot/(mr*volume*fp)
     st.success(f'Nilai Molaritas adalah {molaritas}')
     
 press=st.button("Tampilkan Kesimpulan")
