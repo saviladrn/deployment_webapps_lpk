b0a5486b6713fa70c92745cc8c5f0e507ae98384
saviladrn <132325574+saviladrn@users.noreply.github.com>
2023-05-12 11:30:43 +0700

    Update molaritas.py

--- molaritas.py
+++ molaritas.py
@@ -8,7 +8,7 @@ image = Image.open("judul.jpeg")
 st.image(image)
 
 st.subheader("Aplikasi Penghitung Molaritas")
-st.title("Molaritas merupakan suatu konsentrasi larutan yang mendefinisikan jumlah mol suatu zat terlarut dalam satu liter larutan. ")
+st.text("Molaritas merupakan suatu konsentrasi larutan yang mendefinisikan jumlah mol suatu zat terlarut dalam satu liter larutan. ")
 
 image = Image.open("molarritas.jpg")
 
